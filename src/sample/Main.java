b36cc2a94d3af73314f51e92f4c661b6d4b77312
faaip <frederikjuutilainen@gmail.com>
2015-04-30 09:45:16 +0200

    Balancer updated with methods for getAngle, isBalanced and isLeaningright

---
+++ src/sample/Main.java
@@ -0,0 +1,73 @@
+package sample;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by frederikjuutilainen on 29/04/15.
+ */
+public class Main {
+
+    private static double amount = 1;
+
+    public static void main(String[] args) {
+
+
+        ExampleGraphics2D world = new ExampleGraphics2D();
+        world.setTitle("Machine Learning");
+
+        // show it
+        world.setVisible(true);
+
+        // start it
+        world.start();
+
+        JFrame gui = new JFrame();
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setSize(300,300);
+
+        // Add controls
+        JButton leanRightButton = new JButton("Lean right");
+        JButton leanLeftButton = new JButton("Left left");
+        JSlider amountSlider = new JSlider(1,10);
+
+        // Panel for controls
+        JPanel controls = new JPanel();
+
+        leanRightButton.addActionListener(e -> {
+            world.bodyList.get(0).lean(-amount);
+        });
+        leanLeftButton.addActionListener(e -> {
+            world.bodyList.get(0).lean(amount);
+        });
+
+        controls.add(leanLeftButton);
+        controls.add(leanRightButton);
+        controls.add(amountSlider);
+
+        // Panel for info monitoring
+        JPanel info = new JPanel();
+        JLabel angleLabel = new JLabel();
+        angleLabel.setText("Angle is: " + world.bodyList.get(0).getAngle());
+
+
+        info.add(angleLabel);
+
+
+        // Panels are added to splitpane
+        JSplitPane split = new JSplitPane();
+        split.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        split.setLeftComponent(info);
+        split.setRightComponent(controls);
+        gui.add(split);
+
+
+//        controls.add(angleLabel);
+//        controls.setPreferredSize(new Dimension(400,400));
+
+
+
+    }
+
+}
