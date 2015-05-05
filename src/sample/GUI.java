1bc027c5f7bbdce574d45fd842416a85e6615cb7
faaip <frederikjuutilainen@gmail.com>
2015-05-05 12:53:44 +0200

    GUI in separate class

---
+++ src/sample/GUI.java
@@ -0,0 +1,59 @@
+package sample;
+
+import javax.swing.*;
+import Rendering_dyn4j.ExampleGraphics2D;
+
+/**
+ * Created by frederikjuutilainen on 05/05/15.
+ */
+public class GUI {
+
+    private static double amount = 1;
+
+
+    public GUI(ExampleGraphics2D world)
+    {
+        JFrame gui = new JFrame();
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setSize(300, 300);
+
+        // Add controls
+        JButton leanRightButton = new JButton("Lean right");
+        JButton leanLeftButton = new JButton("Left left");
+        JSlider amountSlider = new JSlider(1, 10);
+
+        // Panel for controls
+        JPanel controls = new JPanel();
+
+        leanRightButton.addActionListener(e -> {
+            world.balancer.lean(-amount);
+        });
+        leanLeftButton.addActionListener(e -> {
+            world.balancer.lean(amount);
+        });
+
+        amountSlider.addChangeListener(e -> {
+            amount = amountSlider.getValue();
+        });
+
+        controls.add(leanLeftButton);
+        controls.add(leanRightButton);
+        controls.add(amountSlider);
+
+        // Panel for info monitoring
+        JPanel info = new JPanel();
+        JLabel angleLabel = new JLabel();
+        angleLabel.setText("Angle is: " + world.balancer.getAngle());
+
+        info.add(angleLabel);
+
+        // Panels are added to splitpane
+        JSplitPane split = new JSplitPane();
+        split.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        split.setLeftComponent(info);
+        split.setRightComponent(controls);
+        gui.add(split);
+    }
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,8 @@ import Rendering_dyn4j.ExampleGraphics2D;
 
 import javax.swing.*;
 
-/**
- * Created by frederikjuutilainen on 29/04/15.
- */
 public class
         Main {
-    private static double amount = 1;
 
     public static void main(String[] args) {
 
@@ -23,46 +19,9 @@ public class
         // start it
         world.start();
 
-        JFrame gui = new JFrame();
-        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        gui.setVisible(true);
-        gui.setSize(300, 300);
+        // Controls
+        GUI gui = new GUI(world);
 
-        // Add controls
-        JButton leanRightButton = new JButton("Lean right");
-        JButton leanLeftButton = new JButton("Left left");
-        JSlider amountSlider = new JSlider(1, 10);
 
-        // Panel for controls
-        JPanel controls = new JPanel();
-
-        leanRightButton.addActionListener(e -> {
-            world.balancer.lean(-amount);
-        });
-        leanLeftButton.addActionListener(e -> {
-            world.balancer.lean(amount);
-        });
-
-        amountSlider.addChangeListener(e -> {
-            amount = amountSlider.getValue();
-        });
-
-        controls.add(leanLeftButton);
-        controls.add(leanRightButton);
-        controls.add(amountSlider);
-
-        // Panel for info monitoring
-        JPanel info = new JPanel();
-        JLabel angleLabel = new JLabel();
-        angleLabel.setText("Angle is: " + world.balancer.getAngle());
-
-        info.add(angleLabel);
-
-        // Panels are added to splitpane
-        JSplitPane split = new JSplitPane();
-        split.setOrientation(JSplitPane.VERTICAL_SPLIT);
-        split.setLeftComponent(info);
-        split.setRightComponent(controls);
-        gui.add(split);
     }
 }
